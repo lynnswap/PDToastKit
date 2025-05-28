f184b9b483ad7b25c9aca04afc664a198a6692a9
lynnswap <65545348+lynnswap@users.noreply.github.com>
2025-05-28 15:45:42 +0900

    bug fixes

--- Sources/PDToastKit/StackedToastView.swift
+++ Sources/PDToastKit/StackedToastView.swift
@@ -1,34 +1,34 @@
 import SwiftUI
 
 struct StackedToastView: View {
-  var manager: PDToastManager
-  var paddingTop: CGFloat
-  var paddingBottom: CGFloat
-
-  @ViewBuilder
-  private func toastSection(for edge: Edge) -> some View {
-    ForEach(manager.toasts.filter { $0.edge == edge }) { toast in
-      ToastView(
-        type: toast.type,
-        message: toast.message,
-        additionalMessage: toast.additionalMessage,
-        imageUrl: toast.imageUrl,
-        edge: edge
-      )
-      .onTapGesture { manager.removeToast(item: toast) }
+    var manager: PDToastManager
+    var paddingTop: CGFloat
+    var paddingBottom: CGFloat
+    
+    @ViewBuilder
+    private func toastSection(for edge: ToastEdge) -> some View {
+        ForEach(manager.toasts.filter { $0.edge == edge }) { toast in
+            ToastView(
+                type: toast.type,
+                message: toast.message,
+                additionalMessage: toast.additionalMessage,
+                imageUrl: toast.imageUrl,
+                edge: edge
+            )
+            .onTapGesture { manager.removeToast(item: toast) }
+        }
     }
-  }
-
-  var body: some View {
-    VStack {
-      toastSection(for: .top)
-      Spacer()
-      toastSection(for: .bottom)
+    
+    var body: some View {
+        VStack {
+            toastSection(for: .top)
+            Spacer()
+            toastSection(for: .bottom)
+        }
+        .frame(maxWidth: 600)
+        .transition(.opacity)
+        .animation(.bouncy, value: manager.toasts.count)
+        .padding(.top, paddingTop)
+        .padding(.bottom, paddingBottom)
     }
-    .frame(maxWidth: 600)
-    .transition(.opacity)
-    .animation(.bouncy, value: manager.toasts.count)
-    .padding(.top, paddingTop)
-    .padding(.bottom, paddingBottom)
-  }
 }
