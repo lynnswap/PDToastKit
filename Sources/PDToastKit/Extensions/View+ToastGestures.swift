120a0b5c4d6c52fdac0a8ff9965d0b024b1c664d
Kazuki Nakashima <65545348+lynnswap@users.noreply.github.com>
2025-07-28 13:20:39 +0900

    Refactor gesture handling

---
+++ Sources/PDToastKit/Extensions/View+ToastGestures.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+extension View {
+    func toastGestures(for toast: ToastItem, manager: PDToastManager) -> some View {
+        self
+            .onTapGesture {
+                manager.dismiss(toast.id)
+            }
+            .onLongPressGesture(
+                perform: {
+                    manager.pause(toast)
+                },
+                onPressingChanged: { pressing in
+                    if !pressing {
+                        manager.resume(toast)
+                    }
+                }
+            )
+    }
+}
